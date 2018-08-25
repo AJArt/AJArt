84932191ef5fbc828fc8b19afcc4ab302b5577a5
Bryan Stevens <bryanstevens@Bryans-MacBook-Pro.local>
2018-08-25 13:23:49 -0700

    basic website

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 let Next = document.getElementById("next");
 let Prev = document.getElementById("prev");
 let h1 = document.getElementById("num");
+
+let rock = document.getElementById("Rock");
+let hiphop = document.getElementById("Hip Hop");
+let country = document.getElementById("Country");
+
+let object;
+rock.addEventListener('click', function(){
+    buttonClicked('rock');
+}, false);
+hiphop.addEventListener('click', function(){
+    buttonClicked('hip_hop');
+}, false);
+country.addEventListener('click', function(){
+    buttonClicked('country');
+}, false);
+
+function buttonClicked(val){
+    window.location.replace("./?pageNum=1&searchTerm=" + val);
+
+
+}
 let navButtons = document.getElementById("nav");
 let pageNum;
 let APIKey = "AIzaSyC6YNBp5YTz9_VxpwWk3bfhpIaISC05Yq4";
 let googleURL = "https://www.googleapis.com/youtube/v3/search";
 var embedDiv1 = '<div class="player"><h1>';
 var embedDiv2 = '<h2>';
-let embedHTML1 = '"<iframe width="640" height="400" src="https://www.youtube.com/embed/';
-let embedHTML2 = '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>"';
+let embedHTML1 = '<iframe width="640" height="400" src="https://www.youtube.com/embed/';
+let embedHTML2 = '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 
-/*    <iframe width="1280" height="720" src="https://www.youtube.com/embed/9sWEecNUW-o" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> */
 function pageLoad() {
-    pageNum = getURLParam('pageNum',window.location.href);
-    h1.innerHTML = pageNum;
-    console.log(pageNum);
+    object = getURLParam(window.location.href);
+    console.log(object);
+    pageNum = object.pageNum;
+    h1.innerHTML = object.pageNum;
     loadVids();
-    // var idParameter = getUrlParameters("id", url, true);
-    // console.log(idParameter);
 }
 window.onload = pageLoad;
 
 function nextPage (){
     console.log(pageNum);
-    if (pageNum != 10){
+    if (pageNum < 10){
+        console.log('here');
         pageNum++;
-        window.location.replace("http://127.0.0.1:5500/AJArt/?pageNum=" + pageNum);
+        window.location.replace("./?pageNum=" + pageNum);
     }
 }
 function prevPage (){
     console.log(pageNum);
-    if (pageNum != 1){
+    if (pageNum > 1){
         pageNum--;
-        window.location.replace("http://127.0.0.1:5500/AJArt/?pageNum=" + pageNum);
+        window.location.replace("./?pageNum=" + pageNum);
     }
 
 }
@@ -45,35 +65,40 @@ let params = {
 }
 
 
-function loadVids() {
+function loadVids(val) {
+    let search = object.searchTerm;
+        params.q = search;
 
-    let search = "rock music";
-    params.q = search;
-
-
-    $.getJSON(googleURL, params, function (data) {
-        console.log(data);
-        console.log(embedHTML1 + data.items[pageNum].snippet.channelId + embedHTML2);
-        let index = pageNum - 1;
-        $("#video").append(embedHTML1 + data.items[index].id.videoId + embedHTML2);
-    });
+        $.getJSON(googleURL, params, function (data) {
+            console.log(data);
+            let index = pageNum - 1;
+            console.log(embedHTML1 + data.items[index].id.channelId + embedHTML2);
+            $("#video").append(embedHTML1 + data.items[index].id.videoId + embedHTML2);
+        });
 }
 
 
-function getURLParam(param, url){
-    var id = url.substring(url.lastIndexOf('/') + 1);
-    if (id === ""){
-        return 1;
+function getURLParam(url){
+    let object = {};
+    let id = url.substring(url.lastIndexOf('/') + 1);
+    if (url.includes('?')){
+        let arr = id.split('?');
+        let arr1 = arr[1].split('&');
+        for (let i = 0; i < arr1.length; i++){
+            let arr2 = arr1[i].split("=");
+            object[arr2[0]] = arr2[1];
+        }
+        
+        return object;
     }
     else{
-        let arr = id.split('?');
-        let arr1 = arr[1].split('=');
-        return arr1[1];
+        return {'pageNum':1, 'searchTerm':'music'};
     }
 
 }
 
 Next.addEventListener('click', nextPage);
 Prev.addEventListener('click', prevPage);
+
 
 
